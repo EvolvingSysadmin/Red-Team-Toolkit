78decf565758c90f0e2dc671f4a784e43eea955e
EvolvingSysadmin <lebowski.lab@gmail.com>
2020-12-09 16:15:15 -0800

    update sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,9 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'recommonmark', 'sphinx_markdown_tables' ]
+extensions = [ 'sphinx_markdown_tables' ]
+
+# 'recommonmark', 
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
